7f3a0cb4833fd3ed6ea5662424b05d3710d46796
Ritvik11 <36701232+Ritvik11@users.noreply.github.com>
2019-10-17 22:12:36 +0530

    Added Visual Distribution

---
+++ Assets/Scripts/Use/SpawnCards.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCards : MonoBehaviour
+{
+    public GameObject CardPrefab;
+    public GameObject newCard;
+
+    private void Start()
+    {
+        
+
+        newCard = Instantiate(CardPrefab);
+
+        newCard.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/cards/CardBack");
+
+
+        
+        
+    }
+}
--- Assets/Scripts/Use/_GameManager.cs
+++ Assets/Scripts/Use/_GameManager.cs
@@ -6,28 +6,61 @@ public class _GameManager : MonoBehaviour
 {
     List<Player> players;
     Cards cards;
-    SpriteRenderer renderer;
+    
     Sprite sprite;
     public int playerCount;
-   
-    
+    GameObject go;
+    SpawnCards spawnCards;
+    Vector3 position;
+  public  GameObject CardPrefab;
+    public float speed;
+    float step;
 
-  private void GiveHand()
+    private void GiveHand(int round)
     {
+        int i = 1;
         foreach(Player player in players)
         {
             player.hand.Add(cards.DeckOfCards[0]);
-            cards.DeckOfCards.RemoveAt(0);
+
+                GameObject newCard;
+              
+
+            newCard = Instantiate(CardPrefab);
+
+            newCard.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/cards/" + cards.DeckOfCards[0].CardName);
+               
+                position =  GameObject.Find("Seat" + i).transform.position;
+                Debug.Log(position);
+
+            newCard.transform.position = Vector3.MoveTowards(transform.position, position, step);
+                
+            if(round == 1)
+            {
+                Vector3 newPosition = new Vector3(position.x + 1f, position.y, position.z);
+                newCard.transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
+            }
+           
+
+            SpriteRenderer renderer =  newCard.GetComponent<SpriteRenderer>();
+                renderer.sprite = newCard.GetComponent<SpriteRenderer>().sprite;
+                renderer.sortingLayerName = "Cards";
+
             
+
+            cards.DeckOfCards.RemoveAt(0);
+            i++;
         }
     }
 
     private void Start()
     {
+        step = speed * Time.deltaTime * 10f;
+        spawnCards = FindObjectOfType<SpawnCards>();
         cards = FindObjectOfType<Cards>();
         players = new List<Player>();
 
-        renderer = gameObject.GetComponent<SpriteRenderer>();
+        
         sprite = GetComponent<Sprite>();
 
         for (int i = 0; i < playerCount; i++)
@@ -37,21 +70,11 @@ public class _GameManager : MonoBehaviour
 
        
 
-        sprite = Resources.Load<Sprite>("Sprites/cards/3_14");
-        renderer.sprite = sprite;
-      //  renderer.sortingLayerName = "Cards";
-        Instantiate(gameObject.GetComponent<SpriteRenderer>().sprite = sprite, transform.position, Quaternion.identity);
-
-        for (int i = 0; i < 2; i++)
-            GiveHand();
-
-        //SpriteRenderer renderer = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
-        // card =  Instantiate(Resources.Load<Sprite>("Sprites/cards/CardBack"), transform.position, Quaternion.identity );
-        // renderer.sprite = card;
-        //   gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/cards/CardBack");
        
-        //  gameObject.transform.localScale = new Vector3(46.84328f, 46.84328f, 46.84328f);
+        for (int i = 0; i < 2; i++)
+            GiveHand(i);
 
+        
         GiveCommCards();
         PrintStatus();
     }
@@ -77,5 +100,10 @@ public class _GameManager : MonoBehaviour
         for (int i = 0; i < cards.CommunityCards.Count; i++)
             Debug.Log(cards.CommunityCards[i].CardName);
     }
+
+   
+   
+
+   
 
 }
